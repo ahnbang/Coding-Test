f5daa0287c3b0248ac517f7498750f29cbaa629b
Ahn Byoung Joon <97154684+ahnbang@users.noreply.github.com>
2022-04-13 15:28:32 +0900

    Add files via upload

---
+++ [2022 Kakao Blind Recruitment]/[신고결과받기].py
@@ -0,0 +1,24 @@
+def solution(id_list, report, k):
+    answer = [0]*len(id_list)
+    count=[0]*len(id_list)
+    dic ={}
+    for i in id_list:
+        dic[i] = []
+    
+    for i in report:
+        report = i.split()
+        if dic[report[0]] != [report[1]]: 
+            dic[report[0]].append(report[1])
+
+    for i in range(len(id_list)):
+        id_list[i]
+        for ID in id_list:
+            if id_list[i] in dic[ID]:
+                count[i]+=1
+
+    for i in range(len(id_list)):
+        if count[i] >= k:
+            for j in range(len(id_list)):
+                if id_list[i] in dic[id_list[j]]:
+                    answer[j]+=1
+    return answer
